0876cb9cac2d4b5d33500433d6ff61f41e0a501c
Jesus G <jgaitan345@gmail.com>
2025-07-08 11:54:27 -0500

    feat: most exercises 1c

--- part1/example/example/src/App.jsx
+++ part1/example/example/src/App.jsx
@@ -1,23 +1,41 @@
-const Hello = ({ name, age }) => {
-    const bornYear = () => new Date().getFullYear() - age
+import { useState } from 'react'
 
+const Display = ({ counter }) => {
     return (
-        <div>
-            <p>Hello {name}, you are {age} years old</p>
-            <p>So you were probably born in {bornYear()}</p>
-        </div>
+        <div>{counter}</div>
+    )
+}
+
+const Button = (props) => {
+    return (
+        <button onClick={props.onClick}>
+            {props.text}
+        </button>
     )
 }
 
 const App = () => {
-    const name = 'Peter'
-    const age = 10
+    const [ counter, setCounter ] = useState(0)
+
+    const increaseByOne = () => setCounter(counter + 1)
+    const decreaseByOne = () => setCounter(counter - 1)
+    const setToZero = () => setCounter(0)
 
     return (
         <div>
-            <h1>Greetings</h1>
-            <Hello name="Pedro" age={26 + 10} />
-            <Hello name={name} age={age} />
+            <Display counter={counter}/>
+            <Button
+                onClick={increaseByOne}
+                text='plus'
+            />
+            <Button
+                onClick={setToZero}
+                text='zero'
+            />
+            <Button
+                onClick={decreaseByOne}
+                text='minus'
+            />
         </div>
     )
 }
